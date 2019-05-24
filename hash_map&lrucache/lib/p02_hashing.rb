72212e7e510f09f5f97f18c6ff88ee80ee7f2214
mwdeshaw <mwdeshaw@gmail.com>
2019-05-24 16:37:28 -0700

    Add int_set and hashing

--- hash_map&lrucache/lib/p02_hashing.rb
+++ hash_map&lrucache/lib/p02_hashing.rb
@@ -1,21 +1,53 @@
+require "byebug"
+
 class Integer
   # Integer#hash already implemented for you
 end
 
 class Array
   def hash
+    sum = 0
+    self.each_with_index do |el,idx| 
+      sum += el*idx
+    end
+    sum
   end
 end
 
 class String
   def hash
+    alpha = ("a".."z").to_a
+    sum = 0
+    self.each_char.with_index do |char, idx|
+        sum += (idx * alpha.index(char.downcase))
+    end
+
+    sum
   end
+
 end
 
 class Hash
+
+
+
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    crypto = { 
+    :a => 7,
+    :b => 2
+  }
+    alpha = ("a".."z").to_a
+    sum = 0
+
+    self.each do |k,v|
+      if alpha.include?(v)
+        sum += crypto[k] * alpha.index(v)
+      else
+        sum += crypto[k] * v
+      end
+    end
+    sum
   end
 end
