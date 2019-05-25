19031a20f055703bb414e9a6de519adc1257b6a1
mwdeshaw <mwdeshaw@gmail.com>
2019-05-24 18:11:54 -0700

    Work on hash sets

--- hash_map&lrucache/lib/p03_hash_set.rb
+++ hash_map&lrucache/lib/p03_hash_set.rb
@@ -1,3 +1,5 @@
+require_relative "p02_hashing.rb"
+
 class HashSet
   attr_reader :count
 
@@ -10,6 +12,8 @@ class HashSet
   end
 
   def include?(key)
+    arr = @store.flatten
+    arr.include?(self[key])
   end
 
   def remove(key)
@@ -19,6 +23,19 @@ class HashSet
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    new_num = 0
+    case num
+    when num.is_a?(Hash)
+      new_num = Hash.hash(num)
+    when num.is_a?(String)
+      new_num = String.hash(num)
+    when num.is_a?(Array)
+      new_num = Array.hash(num)
+    when num.is_a?(Integer)
+      new_num = Integer.hash
+    end
+
+    self.store[new_num % num_buckets]
   end
 
   def num_buckets
